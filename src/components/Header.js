a6c27b8b9b7150ee7b5ca04c583fd7a787fcb562
ChaseLittlepaws <littlepaws@hey.com>
2023-03-24 22:53:08 -0400

    Replaced manual data entry for header with mapped data.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import {
   container,
-  heading,
   navLinks,
   navLinkItem,
   navLinkText
@@ -15,62 +14,56 @@ import storeIcon from '/src/images/storeIcon.png';
 import socialsIcon from '/src/images/socialsIcon.png';
 
 const Header = ({ }) => {
+  const data = [
+    {
+      name: "Photos",
+      nav: "/photos",
+      icon: photosIcon,
+    },
+    {
+      name: "Favorites",
+      nav: "/favorites",
+      icon: favoritesIcon,
+    },
+    {
+      name: "Stories",
+      nav: "/stories",
+      icon: storiesIcon,
+    },
+    {
+      name: "Blogs",
+      nav: "/blogs",
+      icon: blogsIcon,
+    },
+    {
+      name: "Store",
+      nav: "/store",
+      icon: storeIcon,
+    },
+    {
+      name: "Socials",
+      nav: "/socials",
+      icon: socialsIcon,
+    }
+  ]
+
   return (
-    <><div className={container}>
-          <nav>
-              <ul className={navLinks}>
-                  <li className={navLinkItem}>
-                      <Link to="/photos" className={navLinkText}>
-                          <img src={photosIcon} alt="" style={{ width: '105px', height: 'auto' }} />
-                      </Link>
-                      <Link to="/photos" className={navLinkText}>
-                          Photos
-                      </Link>
-                  </li>
-                  <li className={navLinkItem}>
-                      <Link to="/favorites" className={navLinkText}>
-                          <img src={favoritesIcon} alt="" style={{ width: '105px', height: 'auto' }} />
-                      </Link>
-                      <Link to="/favorites" className={navLinkText}>
-                          Favorites
-                      </Link>
-                  </li>
-                  <li className={navLinkItem}>
-                      <Link to="/stories" className={navLinkText}>
-                          <img src={storiesIcon} alt="" style={{ width: '105px', height: 'auto' }} />
-                      </Link>
-                      <Link to="/stories" className={navLinkText}>
-                          Stories
-                      </Link>
-                  </li>
-                  <li className={navLinkItem}>
-                      <Link to="/blogs" className={navLinkText}>
-                          <img src={blogsIcon} alt="" style={{ width: '105px', height: 'auto' }} />
-                      </Link>
-                      <Link to="/blogs" className={navLinkText}>
-                          Blogs
-                      </Link>
-                  </li>
-                  <li className={navLinkItem}>
-                      <Link to="/store" className={navLinkText}>
-                          <img src={storeIcon} alt="" style={{ width: '105px', height: 'auto' }} />
-                      </Link>
-                      <Link to="/store" className={navLinkText}>
-                          Store
-                      </Link>
-                  </li>
-                  <li className={navLinkItem}>
-                      <Link to="/socials" className={navLinkText}>
-                          <img src={socialsIcon} alt="" style={{ width: '105px', height: 'auto' }} />
-                      </Link>
-                      <Link to="/socials" className={navLinkText}>
-                          Socials
-                      </Link>
-                  </li>
-              </ul>
-          </nav>
-      </div>
-      </>
+    <div className={container}>
+      <nav>
+        <ul className={navLinks}>
+          {data.map(({ name, nav, icon }) => (
+            <li className={navLinkItem} key={name}>
+              <Link to={nav} className={navLinkText}>
+                <img src={icon} alt={name} style={{ width: '105px', height: 'auto' }} />
+              </Link>
+              <Link to={nav} className={navLinkText}>
+                {name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   )
 }
 
