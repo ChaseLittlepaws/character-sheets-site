0b2275f53ec676ea768f21e951eb4f4bfef208a9
ChaseLittlepaws <littlepaws@hey.com>
2023-07-28 18:40:22 -0400

    Added Google Fonts to the bio component.

--- src/components/bio.js
+++ src/components/bio.js
@@ -2,15 +2,18 @@ import React from "react";
 import { container,
         content,
         h1Style,
-        pStyle
-    } from '../styles/header.module.css'
+        pStyle,
+        bioText,
+    } from '../styles/bio.module.css'
 
 function Bio() {
     return (
       <div className={container}>
         <div className={content}>
-            <h1 className={h1Style}>Hello, world!</h1>
-            <p className={pStyle}>Welcome to my website.</p>
+            <div className={bioText}>
+                <h1 className={h1Style}>Hello, world!</h1>
+                <p className={pStyle}>Welcome to my website.</p>
+            </div>
         </div>
       </div>
     );
