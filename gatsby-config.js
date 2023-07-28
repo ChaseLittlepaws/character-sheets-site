bdc0e2c6660f34e1c21c82eac2c0ef3aeda1e524
ChaseLittlepaws <littlepaws@hey.com>
2023-07-28 19:52:32 -0400

    Added netilfy module

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `Character Sheets Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", `gatsby-plugin-netlify`, {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
