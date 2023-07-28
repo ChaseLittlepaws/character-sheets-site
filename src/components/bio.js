b234b8028cbc20cd862f3d1730daf94b744f9f23
ChaseLittlepaws <littlepaws@hey.com>
2023-07-28 17:58:29 -0400

    Testing bio component

---
+++ src/components/bio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { container } from '../styles/bio.module.css'
+
+function Bio() {
+    return (
+      <div className={container}>
+        <h1>Hello, world!</h1>
+        <p>Welcome to my website.</p>
+      </div>
+    );
+}
+
+export default Bio;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/header.js';
 import Layout from '../components/layout.js';
+import Bio from '../components/bio.js';
 
 function App() {
   return (
     <Layout>
       <div>
         <Header />
+        <Bio />
       </div>
     </Layout>
   );
